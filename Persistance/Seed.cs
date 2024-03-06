93f04f96e82e58bf35dd7961f3b075362ba782a7
Alin Cristian <alin.cristian977@gmail.com>
2024-03-06 20:51:19 +0200

    End of Section 2

---
+++ Persistance/Seed.cs
@@ -0,0 +1,75 @@
+﻿using Domain;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System;
+
+namespace Persistance
+{
+    public class Seed
+    {
+        public static async Task SeedData(DataContext context)
+        {
+            if (context.Activities.Any()) return;
+
+            var activities = new List<Activity>
+            {
+                new Activity
+                {
+                    Title = "Past Activity 1",
+                    Date = DateTime.Now.AddMonths(-2),
+                    Description = "Activity 2 months ago",
+                    Category = "drinks",
+                    City = "London",
+                    Venue = "Pub"
+                },
+                new Activity
+                {
+                    Title = "Past Activity 2",
+                    Date = DateTime.Now.AddMonths(-1),
+                    Description = "Activity 1 month ago",
+                    Category = "culture",
+                    City = "Paris",
+                    Venue = "Louvre"
+                },
+                new Activity
+                {
+                    Title = "Future Activity 1",
+                    Date = DateTime.Now.AddMonths(1),
+                    Description = "Activity 1 month in the future",
+                    Category = "culture",
+                    City = "London",
+                    Venue = "Natural History Museum"
+                },
+                new Activity
+                {
+                    Title = "Future Activity 2",
+                    Date = DateTime.Now.AddMonths(2),
+                    Description = "Activity 2 months in the future",
+                    Category = "music",
+                    City = "London",
+                    Venue = "02 Arena"
+                },
+                new Activity
+                {
+                    Title = "Future Activity 3",
+                    Date = DateTime.Now.AddMonths(3),
+                    Description = "Activity 3 months in the future",
+                    Category = "drinks",
+                    City = "London",
+                    Venue = "Another pub"
+                },
+                new Activity
+                {
+                    Title = "Future Activity 5",
+                    Date = DateTime.Now.AddMonths(5),
+                    Description = "Activity 5 months in the future",
+                    Category = "drinks",
+                    City = "London",
+                    Venue = "Just another pub"
+                }
+            };
+
+            await context.Activities.AddRangeAsync(activities);
+            await context.SaveChangesAsync();
+        }
+    }
+}
